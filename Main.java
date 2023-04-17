7ec0504dc69f9f8f46e7bc797d2082753a3fb7e8
yevgeniy.kryukov.1984 <yevgeniy.kryukov.1984@gmail.com>
2023-04-17 13:05:46 +0600

    refactoring

--- Main.java
+++ Main.java
@@ -1,60 +1,40 @@
-package count_repeat_array_elems; 
+package count_repeat_array_elems;
 
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		final String[] arr = {"a1", "a2", "a3", "a1", "a3", "a10", "a1", "a55", "a55", "a55", "a23", "a23", "b1", "b1", "b1", "b1", "a11", "a12"};
-		
-		Map<String,Integer> map = new HashMap<>();
-		
-		//Groupping elements
-		Integer val;
-		Integer valNew = 0;
-		for(String el : arr) {
-			val = map.get(el);
-			if (val == null) valNew = 1;
-			else valNew = val + 1;
-			map.put(el, valNew);
-			//System.out.println(el);
-		}
-		
-		System.out.println("Groupping elements:");
-		for (Map.Entry<String,Integer> item : map.entrySet()) {
-			System.out.printf(" Key: %s Value: %s \n", item.getKey(), item.getValue());
-		}
+    public static void run(final String[] arr) {
+        // Counting dublicates
+        Map<String, Integer> map1 = Arrays.stream(arr)
+                .sorted()
+                .collect(LinkedHashMap::new,
+                        (map, key) -> map.merge(key, 1, Integer::sum),
+                        LinkedHashMap::putAll);
 
-		//Filter unique elements
-		Map<String,Integer> mapUniq = new LinkedHashMap<>();
-		map.entrySet().stream()
-			//.filter(el->el.getValue()>1)
-			.filter(el->el.getValue()==1)
-			//.sorted(new KeyComparator<String,Integer>())
-			.sorted(Map.Entry.comparingByKey())
-			//.sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
-			.forEach(el->mapUniq.put(el.getKey(), el.getValue()));
-	
-		System.out.println("Unique elements:");
-		for (Map.Entry<String,Integer> item : mapUniq.entrySet()) {
-			System.out.printf(" Key: %s Value: %s \n", item.getKey(), item.getValue());
-		}	
-		
-	}
-}
-class KeyComparator<K, V extends Comparable<V>> implements Comparator<Map.Entry<K, V>> {
-	public int compare(Map.Entry<K, V> a, Map.Entry<K, V> b) {
-		Integer v1 = (Integer) a.getValue();
-		Integer v2 = (Integer) b.getValue();
-		
-		String k1 = (String) a.getKey();
-		String k2 = (String) b.getKey();
+        System.out.println("Dublicate elements:");
+        map1.forEach((key, value) -> {
+            if (value > 1) {
+                System.out.printf(" Key: %s Value: %s \n", key, value);
+            }
+        });
 
-		if (v1 != v2) {
-			return v2 - v1;
-		}
-		
-		return k1.toUpperCase().compareTo(k2.toUpperCase());
-		
-		//return a.getValue().compareTo(b.getValue());
-	}
+        // Filter unique elements
+        System.out.println("Unique elements:");
+        map1.forEach((key, value) -> {
+            if (value == 1) {
+                System.out.printf(" Key: %s Value: %s \n", key, value);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------");
+        String[] arr = {"a1", "a2", "a3", "a1", "a3", "a10", "a1", "a55", "a55", "a55", "a23", "a23", "b1", "b1", "b1", "b1", "a11", "a12"};
+        System.out.println(Arrays.toString(arr));
+        Main.run(arr);
+        System.out.println("--------------------------------------------");
+        arr = new String[]{"a1", "a2", "a3"};
+        System.out.println(Arrays.toString(arr));
+        Main.run(arr);
+    }
 }
